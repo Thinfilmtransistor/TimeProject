82616be855b7228d926534e5245f7cb905f7cff7
Thinfilmtransistor <35184569+Thinfilmtransistor@users.noreply.github.com>
2019-01-25 21:59:21 +0900

    Add files via upload
    delete sleep time
    I'm just modify my computer time more exactly

--- mouse.py
+++ mouse.py
@@ -7,25 +7,24 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 
-print(time.time())
-print(time.ctime())
-print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'))
-
-# print(time.strftime("%H:%M:%S.%f",time.localtime()))
-# print(time.clock_gettime_ns())
-
 while True:
     minute = int(datetime.utcnow().strftime('%M'))
     second = float(datetime.utcnow().strftime('%S.%f'))
-    if minute == 0:
+    if minute == 23:
         print("before",minute, second)
-        time.sleep(0.6729) # 0.673 ~ 0.6735
+        # time.sleep(0.5) # 0.673 ~ 0.6735
         # time.sleep(0.673) # 0.673 ~ 0.6735
         click(1880, 960)
-
-        minute = int(datetime.utcnow().strftime('%M'))
-        second = float(datetime.utcnow().strftime('%S.%f'))
-        print("after",minute, second)
         break
     else:
         print(second)
+
+minute = int(datetime.utcnow().strftime('%M'))
+second = float(datetime.utcnow().strftime('%S.%f'))
+print("after",minute, second)
+now = str(datetime.now())
+f = open('C:/Python/project/MakeLog.txt', "a")
+f.write("\n"+now)
+
+f.close()
+
